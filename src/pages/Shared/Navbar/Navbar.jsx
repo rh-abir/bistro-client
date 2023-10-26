4412be4a99ee79a7d22097af5cbac6ec07766d4c
rh-abir <rhabir.co@gmail.com>
2023-10-27 00:08:19 +0600

    add banner slider

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-black max-w-screen-xl text-white bg-opacity-20 fixed z-10">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
