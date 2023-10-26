7a300bbb7ada83e6d9babeba3976c399aa58fa36
rh-abir <rhabir.co@gmail.com>
2023-10-26 17:20:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h2 className="text-center text-blue-500 underline">This is text</h2>
+    </div>
+  );
+};
+
+export default App;
